82bf5266e06ac5bf1790708300c259260bd9c909
Lucas Kobayashi <kobacarmo2@hotmail.com>
2024-01-07 16:13:30 -0300

    Inicio do projeto

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require('cors')
+
+const db = require("./database")
+const routes = requier("./routes")
+
+const app = express()
+
+// Conexão com o banco
+db.conenct();
+
+const allowedOrigins = [
+    'http://127.0.0.1:5500'
+]
+
+// habilitando CORS
+app.use(cors({
+    origin: function(origin, callback){
+        let allowed = true
+
+        // mobile
+        if(!origin) allowed = true
+
+        if(!allowedOrigins.includes(origin)) allowed = false
+        
+        callback(null,allowed)
+    }
+}))
+
+// Habilita server a receber json
+app.use(express.json())
+
+// Definindo as rotas
+app.use("/api", routes)
+
+// Executando o servidor
+const port = process.env.port || 8080
+app.listen(port, () => console.log(`Server listening to port ${port}`))
