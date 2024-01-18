9666af6cfe275ec3435e0df0c7acb45095fede8d
Lucas Kobayashi <kobacarmo2@hotmail.com>
2024-01-17 22:48:28 -0300

    iniciando rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,10 @@ const router = require('express').Router()
 
 const CustomerController = require('../controllers/customers')
 
+router.get('/customers', (req, res) =>{
+    res.send({
+        ok:123
+    })
+})
+
 module.exports = router
