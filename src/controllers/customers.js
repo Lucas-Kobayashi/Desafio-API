5acac98d0dc90c79d1abe89710170f56e82db9f9
Lucas Kobayashi <kobacarmo2@hotmail.com>
2024-02-10 12:36:25 -0300

    post do cliente

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -1,11 +1,35 @@
-const CustomersModels = require('../models/customers')
+const CustomersModel = require('../models/customers')
 
 async function get(req, res) {
-    const customers = await CustomersModels.find()
+    const customers = await CustomersModel.find()
 
     res.send(customers)
 }
 
+async function post(req, res) {
+    const {
+        name,
+        email,
+        phone,
+        address
+    } = req.body
+
+    const customer = new CustomersModel({
+        name,
+        email,
+        phone,
+        address
+    })
+
+    customer.save()
+
+
+    res.send({
+        message: "success"
+    })
+}
+
 module.exports = {
     get,
+    post
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router()
 const customerController = require('../controllers/customers')
 
 router.get('/customers', customerController.get)
+router.post('/customers', customerController.post)
 
 module.exports = router
