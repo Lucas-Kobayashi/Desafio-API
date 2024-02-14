00f5cc5d697ac2f119462608ef2604a1c5519bb7
Lucas Kobayashi <kobacarmo2@hotmail.com>
2024-02-14 19:18:00 -0300

    post de produtos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
 
 const customerController = require('../controllers/customers')
+const productController = require('../controllers/products')
 
 router.get('/customers', customerController.get)
 router.post('/customers', customerController.post)
 router.delete('/customers/:id', customerController.remove)
+
+router.get('/products', productController.get)
 
 module.exports = router
